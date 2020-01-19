c9a2cc15dcf6f6744764df164e423f588b03cddd
reemharbi <reemharbi.3@gmail.com>
2020-01-20 01:08:50 +0300

    Create alert message

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,12 +4,12 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, input }) => {
   return (
     <div>
       <p className="f3 white">
-        {"This magic app will detect faces in your pictures. Give it a try!"}
+        {"Detect faces and details in your pictures. Give it a try!"}
       </p>
-      <div className="center">
+      <div className="center grow">
         <div className="pa4 br3 shadow-5 w-70">
           <input className="f4 pa2 w-90 center" type="text" onChange={onInputChange} value={input} />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple mt2" onClick={onButtonSubmit}>
             Detect
           </button>
         </div>

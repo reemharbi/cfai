7acee89461936af121b53d6374d0ff781a92b2ec
reemharbi <reemharbi.3@gmail.com>
2020-11-24 17:49:53 +0300

    Updates

---
+++ precache-manifest.3c761545c834a6654b8553d913997ff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e872366cb89d7f766c320f6c9da603a8",
+    "url": "/cfai/index.html"
+  },
+  {
+    "revision": "29793d56686d44ba9f24",
+    "url": "/cfai/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "798b92798d2d0e7dfc1d",
+    "url": "/cfai/static/css/main.4544e27d.chunk.css"
+  },
+  {
+    "revision": "29793d56686d44ba9f24",
+    "url": "/cfai/static/js/2.c00ceb39.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/cfai/static/js/2.c00ceb39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "798b92798d2d0e7dfc1d",
+    "url": "/cfai/static/js/main.9c27002d.chunk.js"
+  },
+  {
+    "revision": "c23114dedcd9f07b5814",
+    "url": "/cfai/static/js/runtime-main.c596e7db.js"
+  }
+]);
